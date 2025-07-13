d5f1d8465fa9746a6e7356923346dbacb3e114f5
ankith1610 <ankithpissay16@gmail.com>
2025-07-13 19:22:28 +0530

    First backend commit

---
+++ src/routes/auth.route.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {signup,login,logout} from '../controllers/auth.controller.js';
+import {protectRoute} from '../middleware/auth.middleware.js';
+import {onboard} from '../controllers/auth.controller.js';
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout",logout);
+router.post("/onboarding",protectRoute,onboard);
+
+
+export default router;
